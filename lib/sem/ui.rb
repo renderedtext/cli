0104d5a2f02f42f3b1f6fc268b4bd92096845c67
Igor Šarčević <igor@renderedtext.com>
2017-08-01 16:45:09 +0200

    Add help screen

--- lib/sem/ui.rb
+++ lib/sem/ui.rb
@@ -2,7 +2,7 @@ module Sem::UI
   module_function
 
   def strong(message)
-    "\e[31m;#{message}\e[0m"
+    "\e[32m#{message}\e[0m"
   end
 
   def info(message)
@@ -13,6 +13,14 @@ module Sem::UI
     items.each do |item|
       info "  #{item}"
     end
+  end
+
+  def table(rows)
+    list_items = rows.map do |row|
+      row.join(" ")
+    end
+
+    list(list_items)
   end
 
 end
