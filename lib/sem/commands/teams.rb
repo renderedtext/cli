eb929d9cf68f3dc0066e704e0331a20f8aaa6c0a
Igor Šarčević <igor@renderedtext.com>
2017-08-02 12:56:43 +0200

    Add titles and permission field

--- lib/sem/commands/teams.rb
+++ lib/sem/commands/teams.rb
@@ -4,9 +4,10 @@ module Sem
 
       def self.run(_params)
         Sem::UI.table [
-          ["3bc7ed43-ac8a-487e-b488-c38bc757a034", "renderedtext/developers", "72 members"],
-          ["3333c21a-7d64-4f20-b2b1-e044607c33f6", "renderedtext/admins", "10 members"],
-          ["fe3624cf-0cea-4d87-9dde-cb9ddacfefc0", "tb-render/developers", "3 members"]
+          ["ID", "NAME", "PERMISSION", "MEMBERS"],
+          ["3bc7ed43-ac8a-487e-b488-c38bc757a034", "renderedtext/developers", "write", "72 members"],
+          ["3333c21a-7d64-4f20-b2b1-e044607c33f6", "renderedtext/admins", "admin", "10 members"],
+          ["fe3624cf-0cea-4d87-9dde-cb9ddacfefc0", "tb-render/developers", "admin", "3 members"]
         ]
       end
 
