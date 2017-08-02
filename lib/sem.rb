ad27acd34a07129b7900c4af616cbe75e9e74807
Igor Šarčević <igor@renderedtext.com>
2017-08-02 10:57:16 +0200

    Introduce ask UI element

--- lib/sem.rb
+++ lib/sem.rb
@@ -1,3 +1,4 @@
+require "io/console"
 require "sem/version"
 require "terminal-table"
 
--- lib/sem/commands/login.rb
+++ lib/sem/commands/login.rb
@@ -3,14 +3,12 @@ module Sem
     module Login
 
       def self.run(_params)
-        print "Username: "
-        username = STDIN.gets
-
-        print "Password: "
-        STDIN.gets
+        username  = Sem::UI.ask("Username: ")
+        _password = Sem::UI.ask("Password: ", :hidden => true)
 
         Sem::UI.info ""
-        Sem::UI.info "Thanks! #{username}"
+        Sem::UI.info ""
+        Sem::UI.info "Thanks #{username}!"
         Sem::UI.info ""
         Sem::UI.info "Now please go to https://semaphoreci.com/users/edit,"
         Sem::UI.info "fetch your auth token and put it in the ~/.sem/credentials file."
