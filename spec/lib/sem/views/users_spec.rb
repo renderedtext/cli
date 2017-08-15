eded5d32802efe4f09bd42e4c3e6b74e01e15010
Igor Šarčević <igor@renderedtext.com>
2017-08-15 16:07:15 +0200

    List users

---
+++ spec/lib/sem/views/users_spec.rb
@@ -0,0 +1,20 @@
+require "spec_helper"
+
+describe Sem::Views::Users do
+  let(:user) { { :id => "ijovan" } }
+
+  describe ".list" do
+    it "shows list of users" do
+      return_value =
+
+      expected_value = [
+        ["USERNAME"],
+        [user[:id]]
+      ]
+
+      expect(Sem::Views::Users).to receive(:print_table).with(expected_value)
+
+      described_class.list([user])
+    end
+  end
+end
