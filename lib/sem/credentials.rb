1db219c8e3d4bc0e52a9f39ef5c7e530d541a2e4
Jovan Ivanović <jivanovic@renderedtext.com>
2017-08-18 15:53:25 +0200

    Update credentials class

--- lib/sem/credentials.rb
+++ lib/sem/credentials.rb
@@ -4,10 +4,13 @@ module Sem
 
     def self.write(auth_token)
       File.write(PATH, auth_token)
+      File.chmod(0o0600, PATH)
     end
 
     def self.read
-      File.read(PATH).delete("\n")
+      raise Sem::Errors::Auth::NoCredentials unless File.file?(PATH)
+
+      File.read(PATH).strip
     end
   end
 end
--- spec/lib/sem/credentials_spec.rb
+++ spec/lib/sem/credentials_spec.rb
@@ -5,22 +5,44 @@ describe Sem::Credentials do
   let(:auth_token) { "auth_token" }
 
   describe ".write" do
+    before do
+      allow(File).to receive(:write)
+      allow(File).to receive(:chmod)
+    end
+
     it "writes to file" do
       expect(File).to receive(:write).with(path, auth_token)
+
+      described_class.write(auth_token)
+    end
+
+    it "sets permissions on the file" do
+      expect(File).to receive(:chmod).with(0o0600, path)
 
       described_class.write(auth_token)
     end
   end
 
   describe ".read" do
-    before do
-      allow(File).to receive(:read).with(path).and_return(auth_token)
+    context "credentials file exists" do
+      before do
+        allow(File).to receive(:file?).with(path).and_return(true)
+        allow(File).to receive(:read).with(path).and_return(auth_token)
+      end
+
+      it "returns the token" do
+        return_value = described_class.read
+
+        expect(return_value).to eql(auth_token)
+      end
     end
 
-    it "returns the token" do
-      return_value = described_class.read
+    context "credentials file doesn't exist" do
+      before { allow(File).to receive(:file?).with(path).and_return(false) }
 
-      expect(return_value).to eql(auth_token)
+      it "raises the NoCredentials error" do
+        expect { described_class.read }.to raise_exception(Sem::Errors::Auth::NoCredentials)
+      end
     end
   end
 end
