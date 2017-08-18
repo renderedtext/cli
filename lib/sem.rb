63283a68d57d5f48b897bfc1d074d4c3d0a440c7
Igor Šarčević <igor@renderedtext.com>
2017-08-18 14:23:38 +0200

    Top level error handling

--- lib/sem.rb
+++ lib/sem.rb
@@ -7,4 +7,20 @@ module Sem
   require "sem/cli"
   require "sem/api"
   require "sem/views"
+
+  def start(args)
+    Sem::CLI.start(args)
+  rescue Sem::Errors::Auth::NoCredentials
+    puts "[ERROR] You are not logged in."
+    puts ""
+    puts "Please log in with '#{Sem.program_name} login --auth-token <token>'"
+    puts
+    exit 1
+  rescue Sem::Errors::Auth::InvalidCredentils
+    puts "[ERROR] Your credentils are invalid."
+    puts ""
+    puts "Please re-log in with '#{Sem.program_name} login --auth-token <token>'"
+    puts
+    exit 1
+  end
 end
