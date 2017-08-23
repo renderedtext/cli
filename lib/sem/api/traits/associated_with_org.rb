a31820106258966e68252a0db0da4650b35cde7c
Jovan Ivanović <jivanovic@renderedtext.com>
2017-08-23 16:28:09 +0200

    Remove to_h

--- lib/sem/api/traits/associated_with_org.rb
+++ lib/sem/api/traits/associated_with_org.rb
@@ -4,11 +4,13 @@ module Sem
       module AssociatedWithOrg
         PAGES_PER_CALL = 3
 
-        def list_for_org(org_name, query = nil)
+        def list_for_org(org_name, filter_query = nil)
           pages = Sem::Pagination.pages(PAGES_PER_CALL) do |page_index|
-            query = query.to_h.merge(:page => page_index)
+            call_query = { :page => page_index }
 
-            api.list_for_org(org_name, query).to_a
+            call_query.merge!(filter_query) unless filter_query.nil?
+
+            api.list_for_org(org_name, call_query).to_a
           end
 
           pages.flatten.pmap { |instance| to_hash(instance, org_name) }
