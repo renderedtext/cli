fc11e2ab48e0160b003c92aed144d05d0621be64
Igor Šarčević <igor@renderedtext.com>
2017-09-04 18:35:13 +0200

    Implement API action for adding the project

---
+++ lib/sem/api/projects.rb
@@ -0,0 +1,50 @@
+module Sem
+  module API
+    class Projects < Base
+      extend Traits::AssociatedWithOrg
+      extend Traits::AssociatedWithTeam
+
+      class << self
+        def name_to_id(org_name, project_name)
+          info(org_name, project_name)[:id]
+        end
+
+        def list
+          org_names = Orgs.list.map { |org| org[:username] }
+
+          org_names.pmap { |name| list_for_org(name) }.flatten
+        end
+
+        def info(org_name, project_name)
+          project = api.list_for_org(org_name, :name => project_name).first
+
+          raise Sem::Errors::ResourceNotFound.new("Project", [org_name, project_name]) if project.nil?
+
+          to_hash(project, org_name)
+        end
+
+        def create(org_name, args)
+          project = api.create_for_org(org_name, args)
+
+          raise Sem::Errors::ResourceNotCreated.new("Project", [org_name, args[:name]]) if project.nil?
+
+          to_hash(project, org_name)
+        end
+
+        def api
+          client.projects
+        end
+
+        def to_hash(project, org)
+          {
+            :id => project.id,
+            :name => project.name,
+            :org => org,
+            :created_at => project.created_at,
+            :updated_at => project.updated_at
+          }
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/sem/api/projects_spec.rb
@@ -0,0 +1,158 @@
+require "spec_helper"
+
+require_relative "traits/shared_examples_for_associated_with_org"
+require_relative "traits/shared_examples_for_associated_with_team"
+
+describe Sem::API::Projects do
+  let(:class_api) { instance_double(SemaphoreClient::Api::Project) }
+  let(:client) { instance_double(SemaphoreClient, :projects => class_api) }
+
+  let(:org_name) { "org" }
+  let(:team_name) { "team" }
+
+  let(:instance_id) { 0 }
+  let(:instance_name) { "project" }
+  let(:instance_hash) do
+    {
+      :id => instance_id,
+      :name => instance_name,
+      :org => org_name,
+      :created_at => 123,
+      :updated_at => 456
+    }
+  end
+
+  let(:instance) do
+    instance_double(SemaphoreClient::Model::Project,
+                    :id => instance_id,
+                    :name => instance_name,
+                    :created_at => 123,
+                    :updated_at => 456)
+  end
+
+  before do
+    allow(described_class).to receive(:client).and_return(client)
+    allow(described_class).to receive(:to_hash).and_return(instance_hash)
+  end
+
+  it_behaves_like "associated_with_org"
+  it_behaves_like "associated_with_team"
+
+  describe ".name_to_id" do
+    before { allow(described_class).to receive(:info).with(org_name, instance_name).and_return(instance_hash) }
+
+    it "returns the id" do
+      return_value = described_class.name_to_id(org_name, instance_name)
+
+      expect(return_value).to eql(instance_id)
+    end
+  end
+
+  describe ".list" do
+    let(:org_username) { "org" }
+    let(:org) { { :username => org_username } }
+
+    before do
+      allow(Sem::API::Orgs).to receive(:list).and_return([org])
+      allow(described_class).to receive(:list_for_org).and_return([instance_hash])
+    end
+
+    it "calls list on the sem_api_orgs" do
+      expect(Sem::API::Orgs).to receive(:list)
+
+      described_class.list
+    end
+
+    it "calls list_for_org on the described class" do
+      expect(described_class).to receive(:list_for_org).with(org_username)
+
+      described_class.list
+    end
+
+    it "returns the instance hashes" do
+      return_value = described_class.list
+
+      expect(return_value).to eql([instance_hash])
+    end
+  end
+
+  describe ".info" do
+    before { allow(class_api).to receive(:list_for_org).and_return([instance_hash]) }
+
+    it "calls list_for_org on the class api" do
+      expect(class_api).to receive(:list_for_org).with(org_name, :name => instance_name)
+
+      described_class.info(org_name, instance_name)
+    end
+
+    it "returns the selected instance" do
+      return_value = described_class.info(org_name, instance_name)
+
+      expect(return_value).to eql(instance_hash)
+    end
+
+    context "resource not found" do
+      before { allow(class_api).to receive(:list_for_org).and_return([]) }
+
+      it "raises an exception" do
+        expected_message = "[ERROR] Project lookup failed\n\nProject #{org_name}/#{instance_name} not found."
+
+        expect { described_class.info(org_name, instance_name) }.to raise_exception(Sem::Errors::ResourceNotFound,
+                                                                                    expected_message)
+      end
+    end
+  end
+
+  describe ".create" do
+    let(:args) { { :name => instance_name, :repo_provider => "github", :repo_owner => "renderedtext", :repo_name => "cli" } }
+
+    before { allow(class_api).to receive(:create_for_org).and_return(instance) }
+
+    it "calls create_for_org on the class_api" do
+      expect(class_api).to receive(:create_for_org).with(org_name, args)
+
+      described_class.create(org_name, args)
+    end
+
+    it "converts the instance to instacen hash" do
+      expect(described_class).to receive(:to_hash).with(instance, org_name)
+
+      described_class.create(org_name, args)
+    end
+
+    it "returns the instance hash" do
+      return_value = described_class.create(org_name, args)
+
+      expect(return_value).to eql(instance_hash)
+    end
+
+    context "resource creation failed" do
+      before { allow(class_api).to receive(:create_for_org).and_return(nil) }
+
+      it "raises an exception" do
+        expected_message = "[ERROR] Project creation failed\n\nProject #{org_name}/#{instance_name} not created."
+
+        expect { described_class.create(org_name, args) }.to raise_exception(Sem::Errors::ResourceNotCreated,
+                                                                             expected_message)
+      end
+    end
+  end
+
+  describe ".api" do
+    it "returns the API from the client" do
+      return_value = described_class.api
+
+      expect(return_value).to eql(class_api)
+    end
+  end
+
+  describe ".to_hash" do
+    before { allow(described_class).to receive(:to_hash).and_call_original }
+
+    it "returns the hash" do
+      return_value = described_class.to_hash(instance, org_name)
+
+      expect(return_value).to eql(instance_hash)
+    end
+  end
+end
