839110aeff2b8a6ee8cba292efa1129e454e8794
Igor Šarčević <igor@renderedtext.com>
2017-09-22 12:59:20 +0200

    Singular model names

--- lib/sem/api/base.rb
+++ lib/sem/api/base.rb
@@ -1,15 +1,12 @@
-module Sem
-  module API
-    class Base
-      class << self
-        def client
-          @client ||= SemaphoreClient.new(
-            Sem::Configuration.auth_token,
-            :api_url => Sem::Configuration.api_url,
-            :verbose => (Sem.log_level == Sem::LOG_LEVEL_TRACE)
-          )
-        end
-      end
-    end
+module Sem::API::Base
+
+  def self.client
+    @client ||= SemaphoreClient.new(
+      Sem::Configuration.auth_token,
+      :api_url => Sem::Configuration.api_url,
+      :verbose => (Sem.log_level == Sem::LOG_LEVEL_TRACE),
+      :auto_paginate => true
+    )
   end
+
 end
