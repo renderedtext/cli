f1c8a505625b80b4e666bffdc5cf7e6cd40800be
Igor Šarčević <igor@renderedtext.com>
2017-10-05 12:24:15 +0200

    Dash flags

--- spec/lib/sem/cli_spec.rb
+++ spec/lib/sem/cli_spec.rb
@@ -21,11 +21,11 @@ describe Sem::CLI do
       it "sets the auth token" do
         expect(Sem::Configuration).to receive(:export_auth_token).with("123456")
 
-        sem_run("login --auth_token 123456")
+        sem_run("login --auth-token 123456")
       end
 
       it "displays a success message" do
-        stdout, stderr, status = sem_run("login --auth_token 123456")
+        stdout, stderr, status = sem_run("login --auth-token 123456")
 
         msg = [
           "Your credentials have been saved to #{Sem::Configuration::CREDENTIALS_PATH}."
@@ -41,7 +41,7 @@ describe Sem::CLI do
       before { allow(Sem::Configuration).to receive(:valid_auth_token?).and_return(false) }
 
       it "writes an error to the output" do
-        _stdout, stderr, status = sem_run("login --auth_token 123456")
+        _stdout, stderr, status = sem_run("login --auth-token 123456")
 
         msg = [
           "[ERROR] Token is invalid!"
@@ -54,7 +54,7 @@ describe Sem::CLI do
       it "does not set the auth token" do
         expect(Sem::Configuration).to_not receive(:export_auth_token).with("123456")
 
-        sem_run("login --auth_token 123456")
+        sem_run("login --auth-token 123456")
       end
     end
   end
